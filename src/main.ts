6389ffb9c780de723260f0476f13aedcb1577f0c
Lars Lorenz <mail@larslorenz.dev>
2024-12-20 21:06:36 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https://api.shokuyou.larslorenz.dev'],
+  });
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap();
