661d48bd230fb7804f7db7331de98dae4b154d99
Lars Lorenz <mail@larslorenz.dev>
2024-12-21 00:30:36 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: ['http://localhost:5173', 'https://shokuyou.larslorenz.dev'],
+  });
+  app.enableVersioning({
+    type: VersioningType.URI,
   });
 
   await app.listen(process.env.PORT ?? 3000);
