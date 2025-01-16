f6d7b30021409730cdc1c2e2b69beef6d7488da8
Lars Lorenz <mail@larslorenz.dev>
2025-01-16 17:03:30 +0100

    new endpoints added

--- src/scheduled_meals/dto/update-scheduled_meal.dto.ts
+++ src/scheduled_meals/dto/update-scheduled_meal.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateScheduledMealDto } from './create-scheduled_meal.dto';
 
-export class UpdateScheduledMealDto extends PartialType(CreateScheduledMealDto) {}
+export class UpdateScheduledMealDto extends PartialType(
+  CreateScheduledMealDto,
+) {}
